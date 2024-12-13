20d54b47613a4224a8ba500ab7508efb3d315b98
Asaad Ahmed <asaadbinahmed@gmail.com>
2024-12-13 02:05:40 -0500

    wrote logical OR method as well as tests

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def LOGICAL_AND(a, b):
     return a == 1 and b == 1
 
+def LOGICAL_OR(a, b):
+    return a == 1 or b == 1
+
 def LOGICAL_NOT(a):
     if a == 1:
         return 0
@@ -16,6 +19,10 @@ def isEqual(expected, actual, test):
 def run_tests():
     isEqual(True, LOGICAL_AND(1, 1), "LOGICAL_AND (both 1)")
     isEqual(False, LOGICAL_AND(1, 0), "LOGICAL_AND (both 0)")
+    
+    isEqual(True, LOGICAL_OR(1, 0), "LOGICAL_OR (one is true)")
+    isEqual(False, LOGICAL_OR(0, 0), "LOGICAL_OR (both are false)")
+
     isEqual(0, LOGICAL_NOT(1), "LOGICAL_NOT (1 -> 0)")
     isEqual(1, LOGICAL_NOT(0), "LOGICAL_NOT (0 -> 1)")
     
