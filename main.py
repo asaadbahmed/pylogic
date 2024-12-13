9111e220368e714c81f9688cd306cd6265674fe3
Asaad Ahmed <asaadbinahmed@gmail.com>
2024-12-13 18:42:34 -0500

    wrote a binary to decimal conversion function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+RUN_TESTS = False
+BINARY_VALUES = {0, 1}
+
 def LOGICAL_AND(a, b):
     return a == 1 and b == 1
 
@@ -18,24 +21,58 @@ def LOGICAL_NOT(a):
     elif a == 0:
         return 1
 
-def isEqual(expected, actual, test):
+def DECIMAL_TO_BINARY(value):
+    if value == 1 or value == 0: return value
+    
+    binary_values = []
+    while value != 0:
+        if value % 2 == 0:
+            binary_values.append(1)
+        else:
+            binary_values.append(0)
+        value = value // 2
+
+    # since in decimal -> binary conversion, you go from the least significant bit to the most significant bit
+    binary_values.reverse()
+    print(binary_values)
+
+    return binary_values
+
+def is_equal(expected, actual, test):
     if (expected == actual):
         print("Test passed: " + test)
     else:
         print("Test failed: " + test)
 
 def run_tests():
-    isEqual(1, LOGICAL_AND(1, 1), "LOGICAL_AND (both 1)")
-    isEqual(0, LOGICAL_AND(1, 0), "LOGICAL_AND (both 0)")
+    is_equal(1, LOGICAL_AND(1, 1), "LOGICAL_AND (both 1)")
+    is_equal(0, LOGICAL_AND(1, 0), "LOGICAL_AND (both 0)")
     
-    isEqual(1, LOGICAL_OR(1, 0), "LOGICAL_OR (one is true)")
-    isEqual(0, LOGICAL_OR(0, 0), "LOGICAL_OR (both are false)")
+    is_equal(1, LOGICAL_OR(1, 0), "LOGICAL_OR (one is true)")
+    is_equal(0, LOGICAL_OR(0, 0), "LOGICAL_OR (both are false)")
 
-    isEqual(0, LOGICAL_XOR(1, 1), "LOGICAL_XOR (both are true)")
-    isEqual(0, LOGICAL_XOR(0, 0), "LOGICAL_XOR (both are false)")
-    isEqual(1, LOGICAL_XOR(1, 0), "LOGICAL_XOR (one is true)")
+    is_equal(0, LOGICAL_XOR(1, 1), "LOGICAL_XOR (both are true)")
+    is_equal(0, LOGICAL_XOR(0, 0), "LOGICAL_XOR (both are false)")
+    is_equal(1, LOGICAL_XOR(1, 0), "LOGICAL_XOR (one is true)")
 
-    isEqual(0, LOGICAL_NOT(1), "LOGICAL_NOT (1 -> 0)")
-    isEqual(1, LOGICAL_NOT(0), "LOGICAL_NOT (0 -> 1)")
+    is_equal(0, LOGICAL_NOT(1), "LOGICAL_NOT (1 -> 0)")
+    is_equal(1, LOGICAL_NOT(0), "LOGICAL_NOT (0 -> 1)")
     
-run_tests()
+def main():
+    if RUN_TESTS:
+        run_tests()
+
+    operations = [
+        {"symbol": "&",  "name": "AND", "desc": "A & B"},
+        {"symbol": "+", "name": "OR", "desc": "A + B"},
+        {"symbol": "!", "name": "NOT", "desc": "!A"},
+        {"symbol": "^", "name": "XOR", "desc": "A ^ B"},
+    ]
+
+    print("This is a list of the available operations.\nOperation\tSymbol\tDescription")
+    for info in operations:
+        print(f"{info["name"]}\t\t[{info["symbol"]}]\t{info["desc"]}")
+    
+
+
+main()
