200ced9adf9a02895b48e3fd4bf783e4bac60b5b
Asaad Ahmed <asaadbinahmed@gmail.com>
2024-12-13 23:35:40 -0500

    made the logic functions use shorter syntax and bit-masking so the result is always 0 or1, not True or False

--- main.py
+++ main.py
@@ -3,28 +3,23 @@ RUN_TESTS = False
 def LOGICAL_AND(a, b):
     return a & b
 
-def LOGICAL_XOR(a, b):
-    if a & (not b):
-        return 1
-    elif (not a) & b:
-        return 1
-    else:
-        return 0
-
 def LOGICAL_OR(a, b):
     return a | b
 
+def LOGICAL_XOR(a, b):
+    return a ^ b
+
 def LOGICAL_NAND(a, b):
-    return not (a & b)
+    return ~(a & b) & 1
 
 def LOGICAL_NOR(a, b):
-    return not (a | b)
+    return ~(a | b) & 1
 
 def LOGICAL_XNOR(a, b):
-    return not LOGICAL_XOR(a, b)
+    return ~(a ^ b) & 1
 
 def LOGICAL_NOT(a):
-    return abs(a - 1)
+    return 1 - a
     
 def is_equal(expected, actual, test):
     if (expected == actual or expected is actual):
@@ -49,25 +44,46 @@ def run_tests():
 def main():
     valid_symbols = set({'1', '0', '&', '+', '!', '^', '(', ')', ' '})
     gates = [
-        {"symbol": "&",  "name": "AND", "desc": "A & B", "func": LOGICAL_AND},
-        {"symbol": "+", "name": "OR", "desc": "A + B", "func": LOGICAL_OR},
-        {"symbol": "!", "name": "NOT", "desc": "!A", "func": LOGICAL_NOT},
-        {"symbol": "^", "name": "XOR", "desc": "A ^ B", "func": LOGICAL_XOR},
+        {"symbol": "&",  "name": "AND", "desc": "A & B"},
+        {"symbol": "!&",  "name": "NAND", "desc": "!(A & B)"},
+
+        {"symbol": "+", "name": "OR", "desc": "A + B"},
+        {"symbol": "+", "name": "NOR", "desc": "!(A + B)"},
+
+        {"symbol": "^", "name": "XOR", "desc": "A ^ B"},
+        {"symbol": "^", "name": "XNOR", "desc": "!(A ^ B)"},
+
+        {"symbol": "!", "name": "NOT", "desc": "!A"},        
     ]
 
     a = int(input("Input a bit: "), 2)
     b = int(input("Input another bit: "), 2)
 
-    print("Choose a logic gate.\nGate\t\tSymbol\tDescription")
+    print("Choose a logic gate. The not gate only applies to the first bit you input.\nGate\t\tSymbol\tDescription")
     for info in gates:
         print(f"{info["name"]}\t\t[{info["symbol"]}]\t{info["desc"]}")
     
     choice = input("Choose: ").upper()
-    for gateInfo in gates:
-        if gateInfo["name"] == choice:
-            print("Result: " + str(gateInfo["func"](a, b)))
-            break
+    result = 0
 
+    match choice:
+        case "AND":
+            result = LOGICAL_AND(a, b)
+        case "NAND":
+            result = LOGICAL_NAND(a, b)
+        case "OR":
+            result = LOGICAL_OR(a, b)
+        case "NOR":
+            result = LOGICAL_NOR(a, b)
+        case "XOR":
+            result = LOGICAL_XOR(a, b)
+        case "XNOR":
+            result = LOGICAL_XNOR(a, b)
+        case "NOT":
+            result = LOGICAL_NOT(a)
+            
+    print("Result: " + str(result))
+    
 if RUN_TESTS:
     run_tests()
 else:
