53014fca900476bad28dff8aedc4107a1b693fea
Asaad Ahmed <asaadbinahmed@gmail.com>
2024-12-13 23:12:28 -0500

    implemented basic gate logic

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-RUN_TESTS = True
+RUN_TESTS = False
 
 def LOGICAL_AND(a, b):
-    return a == 1 and b == 1
+    return a & b
 
 def LOGICAL_XOR(a, b):
-    if a == 1 and b == 0:
+    #if a == 1 and b == 0
+    if a & (not b):
         return 1
-    elif a == 0 and b == 1:
+    elif (not a) & b:
         return 1
     else:
         return 0
 
 def LOGICAL_OR(a, b):
-    return a == 1 or b == 1
+    return a | b
 
 def LOGICAL_NOT(a):
-    if a == 1:
-        return 0
-    elif a == 0:
-        return 1
-
+    return abs(a - 1)
+    
 def is_equal(expected, actual, test):
     if (expected == actual or expected is actual):
         print("Test passed: " + test)
@@ -41,17 +39,26 @@ def run_tests():
     is_equal(1, LOGICAL_NOT(0), "LOGICAL_NOT (0 -> 1)")
     
 def main():
-    operations = [
-        {"symbol": "&",  "name": "AND", "desc": "A & B"},
-        {"symbol": "+", "name": "OR", "desc": "A + B"},
-        {"symbol": "!", "name": "NOT", "desc": "!A"},
-        {"symbol": "^", "name": "XOR", "desc": "A ^ B"},
+    valid_symbols = set({'1', '0', '&', '+', '!', '^', '(', ')', ' '})
+    gates = [
+        {"symbol": "&",  "name": "AND", "desc": "A & B", "func": LOGICAL_AND},
+        {"symbol": "+", "name": "OR", "desc": "A + B", "func": LOGICAL_OR},
+        {"symbol": "!", "name": "NOT", "desc": "!A", "func": LOGICAL_NOT},
+        {"symbol": "^", "name": "XOR", "desc": "A ^ B", "func": LOGICAL_XOR},
     ]
 
-    print("This is a list of the available operations.\nOperation\tSymbol\tDescription")
-    for info in operations:
+    a = int(input("Input a bit: "), 2)
+    b = int(input("Input another bit: "), 2)
+
+    print("Choose a logic gate.\nGate\t\tSymbol\tDescription")
+    for info in gates:
         print(f"{info["name"]}\t\t[{info["symbol"]}]\t{info["desc"]}")
     
+    choice = input("Choose: ").upper()
+    for gateInfo in gates:
+        if gateInfo["name"] == choice:
+            print("Result: " + str(gateInfo["func"](a, b)))
+            break
 
 if RUN_TESTS:
     run_tests()
